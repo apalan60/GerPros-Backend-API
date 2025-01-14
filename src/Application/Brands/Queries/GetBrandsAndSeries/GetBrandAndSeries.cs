8be7cc66065d72e4bd54d34518a874cd4e5eb320
apalan60 <apalan60@gmail.com>
2025-01-14 22:00:18 +0800

    add isManager params for brand list

--- src/Application/Brands/Queries/GetBrandsAndSeries/GetBrandAndSeries.cs
+++ src/Application/Brands/Queries/GetBrandsAndSeries/GetBrandAndSeries.cs
@@ -2,7 +2,7 @@
 
 namespace GerPros_Backend_API.Application.Brands.Queries.GetBrandsAndSeries;
 
-public record GetBrandsAndSeriesQuery : IRequest<IEnumerable<BrandDto>>;
+public record GetBrandsAndSeriesQuery(bool IsManager = false) : IRequest<IEnumerable<BrandDto>>;
 
 public class GetBrandsAndSeriesQueryHandler : IRequestHandler<GetBrandsAndSeriesQuery, IEnumerable<BrandDto>>
 {
@@ -16,14 +16,27 @@ public class GetBrandsAndSeriesQueryHandler : IRequestHandler<GetBrandsAndSeries
     public async Task<IEnumerable<BrandDto>> Handle(GetBrandsAndSeriesQuery request,
         CancellationToken cancellationToken)
     {
-        var brandList = await _context.Brands
-            .AsNoTracking()
-            .AsSplitQuery() 
-            .Include(x => x.BrandSeries)
-            .ThenInclude(bs => bs.ProductItems)
-            .Where(x => x.BrandSeries.Any(y => y.ProductItems.Count > 0))
-            .Select(x => x.ToDto())
-            .ToListAsync(cancellationToken);
+        List<BrandDto> brandList;
+        if (request.IsManager)
+        {
+            // For manager, return all brands whenever they have series or product or not
+            brandList = await _context.Brands
+                .AsNoTracking()
+                .Include(x => x.BrandSeries)
+                .Select(x => x.ToDto())
+                .ToListAsync(cancellationToken);
+        }
+        else
+        {
+            brandList = await _context.Brands
+                .AsNoTracking()
+                .AsSplitQuery()
+                .Include(x => x.BrandSeries)
+                .ThenInclude(bs => bs.ProductItems)
+                .Where(x => x.BrandSeries.Any(y => y.ProductItems.Count > 0))
+                .Select(x => x.ToDto())
+                .ToListAsync(cancellationToken);
+        }
 
         return brandList;
     }
